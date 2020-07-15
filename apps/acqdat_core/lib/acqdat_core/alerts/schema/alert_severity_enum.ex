eb1a6ff8b4aa6da4c881420c53a18fcccd75125b
Vikram Singh Jadon <vikramjadon921995@gmail.com>
2020-07-15 11:00:01 +0530

    Added Alerts app directory structure, migration and schema

---
+++ apps/acqdat_core/lib/acqdat_core/alerts/schema/alert_severity_enum.ex
@@ -0,0 +1,9 @@
+import EctoEnum
+
+# Creates an enum for alert severity.
+defenum(AlertSeverityEnum,
+  warning: 0,
+  low: 1,
+  high: 2,
+  severe: 3
+)
---
+++ apps/acqdat_core/lib/acqdat_core/alerts/schema/alert_status_enum.ex
@@ -0,0 +1,7 @@
+import EctoEnum
+
+# Creates an enum for alert severity.
+defenum(AlertStatusEnum,
+  un_resolved: 0,
+  resolved: 1
+)
