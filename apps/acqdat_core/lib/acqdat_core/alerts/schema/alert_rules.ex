650379ca31109f745bdbe6447dbcb3b458091a23
Ayoush <ayoushchourasia1418@gmail.com>
2020-07-20 11:05:03 +0530

    Added alert rules schema

--- apps/acqdat_core/lib/acqdat_core/alerts/schema/alert_rules.ex
+++ apps/acqdat_core/lib/acqdat_core/alerts/schema/alert_rules.ex
@@ -1,9 +1,10 @@
-defmodule AcqdatCore.Alerts.Schema.Alert do
+defmodule AcqdatCore.Alerts.Schema.AlertRules do
   use AcqdatCore.Schema
 
   schema "acqdat_alert_rules" do
     field :entity, :string, null: false
     add :entity_id, :integer, null: false
+    field :policy_name, :string #need to be added in migration
 
     add :project_id, references("acqdat_projects", on_delete: :delete_all), null: false
     add :creator_id, references(:users), on_delete: :delete_all
@@ -16,13 +17,11 @@ defmodule AcqdatCore.Alerts.Schema.Alert do
     add :status, AlertStatusEnum.type()
     add :description, :text
 
-
-    add :rule_parameters, {:array, :map}
-    embeds_many :rule_parameters, RuleParameters, on_replace: :delete do
+    embeds_many :rule_parameters, RuleParameters, on_replace: :delete do #policy parameters will be map
       field(:name, :string, null: false)
       field(:data_type, :string, null: false)
-      field(:entity_parameter_uuid, :string, null: false)
-      field(:entity_parameter_name, :string, null: false)
+      # field(:entity_parameter_uuid, :string, null: false)
+      # field(:entity_parameter_name, :string, null: false)
       field(:value, :string, null: false)
     end
 
