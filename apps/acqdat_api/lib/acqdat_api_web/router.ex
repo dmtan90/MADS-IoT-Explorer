4ba1910530cede2aaddac52a634b55f8cb1c6199
BandanaPandey <bandanapandey11@gmail.com>
2020-10-14 14:44:39 +0530

    Added testcases for project archived api

--- apps/acqdat_api/lib/acqdat_api_web/router.ex
+++ apps/acqdat_api/lib/acqdat_api_web/router.ex
@@ -103,7 +103,10 @@ defmodule AcqdatApiWeb.Router do
     get("/entities", EntityManagement.EntityController, :fetch_all_hierarchy)
 
     get("/projects/:project_id/users", EntityManagement.ProjectController, :fetch_project_users)
-    get("/archived_projects", EntityManagement.ProjectController, :archived)
+
+    get("/archived_projects", EntityManagement.ProjectController, :archived,
+      as: :archived_projects
+    )
 
     resources "/projects", EntityManagement.ProjectController,
       only: [:index, :create, :update, :delete, :show]
