cd79536a4c8f202493cb3acd0265f17b4487c4d8
BandanaPandey <bandanapandey11@gmail.com>
2020-10-15 23:55:58 +0530

    Added extra parameter configuration for all the datacruncher functions components

--- apps/acqdat_core/lib/acqdat_core/data_cruncher/functions/time_series_univariate/timeseries_mean.ex
+++ apps/acqdat_core/lib/acqdat_core/data_cruncher/functions/time_series_univariate/timeseries_mean.ex
@@ -16,7 +16,7 @@ defmodule AcqdatCore.DataCruncher.Functions.TSMean do
   use Virta.Component
   alias AcqdatCore.Repo
 
-  def run(request_id, inport_args, _outport_args, _instance_pid) do
+  def run(request_id, inport_args, _outport_args, _instance_pid, _configuration) do
     data_source = Map.get(inport_args, :ts_datasource)
     result = process_data(data_source)
     {request_id, :reply, %{tsmean: result}}
@@ -35,7 +35,11 @@ defmodule AcqdatCore.DataCruncher.Functions.TSMean do
         {sum, len}
       end)
 
-    (sum / size) |> Float.round(2)
+    if size == 0 do
+      0.0
+    else
+      (sum / size) |> Float.round(2)
+    end
   end
 
   defp process_data(_) do
